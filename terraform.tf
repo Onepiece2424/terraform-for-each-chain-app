7b535074138da475930dec0257a431a743949de1
rioto.nagano <kingdom950927@gmail.com>
2026-06-08 20:37:11 +0900

    add: HCP Terraform の無効化

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  cloud {
-    workspaces {
-      name = "learn-terraform-for-each"
-    }
-  }
+  # cloud {
+  #   workspaces {
+  #     name = "learn-terraform-for-each"
+  #   }
+  # }
 
   required_providers {
     aws = {
