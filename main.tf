e21c93cf0e96ffc2ece05841b80480d5e69e4e3e
rioto.nagano <kingdom950927@gmail.com>
2026-06-08 21:22:24 +0900

    add: EC2インスタンスをモジュールへ移行

--- main.tf
+++ main.tf
@@ -75,8 +75,8 @@ module "elb_http" {
   security_groups = [module.lb_security_group[each.key].security_group_id]
   subnets         = module.vpc[each.key].public_subnets
 
-  number_of_instances = length(aws_instance.app)
-  instances           = aws_instance.app.*.id
+  number_of_instances = length(module.ec2_instances[each.key].instance_ids)
+  instances           = module.ec2_instances[each.key].instance_ids
 
   listener = [{
     instance_port     = "80"
@@ -94,37 +94,18 @@ module "elb_http" {
   }
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
+module "ec2_instances" {
+  source     = "./modules/aws-instance"
+  depends_on = [module.vpc]
 
-resource "aws_instance" "app" {
-  count = 2
+  for_each = var.project
 
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
+  instance_count     = each.value.instances_per_subnet * length(module.vpc[each.key].private_subnets)
+  instance_type      = each.value.instance_type
+  subnet_ids         = module.vpc[each.key].private_subnets[*]
+  security_group_ids = [module.app_security_group[each.key].security_group_id]
 
-  subnet_id              = module.vpc.private_subnets[0]
-  vpc_security_group_ids = [module.app_security_group.security_group_id]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
-    EOF
-
-  tags = {
-    Terraform   = "true"
-    Project     = var.project_name
-    Environment = var.environment
-  }
+  project_name = each.key
+  environment  = each.value.environment
 }
