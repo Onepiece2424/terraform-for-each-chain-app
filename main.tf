a557c5a9670a1a84fa05b9ff3edf54c9d371a867
rioto.nagano <kingdom950927@gmail.com>
2026-06-08 20:55:17 +0900

    add: ロードバランサーとセキュリティグループの設定更新

--- main.tf
+++ main.tf
@@ -46,10 +46,12 @@ module "lb_security_group" {
   source  = "terraform-aws-modules/security-group/aws//modules/web"
   version = "4.9.0"
 
-  name = "load-balancer-sg-${var.project_name}-${var.environment}"
+  for_each = var.project
+
+  name = "load-balancer-sg-${each.key}-${each.value.environment}"
 
   description = "Security group for load balancer with HTTP ports open within VPC"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id      = module.vpc[each.key].vpc_id
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
 }
@@ -63,13 +65,15 @@ module "elb_http" {
   source  = "terraform-aws-modules/elb/aws"
   version = "3.0.1"
 
+  for_each = var.project
+
   # Comply with ELB name restrictions
   # https://docs.aws.amazon.com/elasticloadbalancing/2012-06-01/APIReference/API_CreateLoadBalancer.html
-  name     = trimsuffix(substr(replace(join("-", ["lb", random_string.lb_id.result, var.project_name, var.environment]), "/[^a-zA-Z0-9-]/", ""), 0, 32), "-")
+  name     = trimsuffix(substr(replace(join("-", ["lb", random_string.lb_id.result, each.key, each.value.environment]), "/[^a-zA-Z0-9-]/", ""), 0, 32), "-")
   internal = false
 
-  security_groups = [module.lb_security_group.security_group_id]
-  subnets         = module.vpc.public_subnets
+  security_groups = [module.lb_security_group[each.key].security_group_id]
+  subnets         = module.vpc[each.key].public_subnets
 
   number_of_instances = length(aws_instance.app)
   instances           = aws_instance.app.*.id
